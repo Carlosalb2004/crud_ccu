6ad0e64c054e61001890ec9428c8f2031e0796f4
JLaurenteT <josue.laurente@ucsm.edu.pe>
2022-11-03 23:46:31 -0500

    css

--- registration-patients.php
+++ registration-patients.php
@@ -1,94 +1,99 @@
 <?php
-    include("config.php");                     //llamamos al archivo config.php ya que tiene la variable $msqli
-    $sql = "SELECT * FROM patients";
-    $query = mysqli_query($mysqli, $sql);       //asignamos la variable $query con la funcion mysqli_query
+include("config.php");                     //llamamos al archivo config.php ya que tiene la variable $msqli
+$sql = "SELECT * FROM patients";
+$query = mysqli_query($mysqli, $sql);       //asignamos la variable $query con la funcion mysqli_query
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Registro de Pacientes</title>
-        <link rel="stylesheet" type="text/css" href="style-patients.css?ts=<?=time()?>" />
-        <meta name=""viewport content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
-    
-    </head>
-    <div>
-    <?php
-include("header.php"); 
-    ?>
-</div>  
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Registro de Pacientes</title>
+    <link rel="stylesheet" type="text/css" href="style-patients.css?ts=<?= time() ?>" />
+    <meta name="" viewport content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
 <div>
+    <?php
+    include("header.php");
+    ?>
+</div>
+<div>
+
     <body>
-          
-    <div class="users-form">
-        <form action="register-patients.php" method="POST"> 
+
+        <div class="users-form">
+            <form action="register-patients.php" method="POST">
                 <h1>Ingresar datos del Paciente</h1>
                 <input type="text" name="dni" placeholder="Dni" required>
                 <input type="text" name="name" placeholder="Nombre" required>
                 <input type="text" name="phone_number" placeholder="Numero Telefonico" required>
-                <input type="text" name="address" placeholder="Direccion" required>              
-                <input type="text" name="email" placeholder="E-mail" required>           
+                <input type="text" name="address" placeholder="Direccion" required>
+                <input type="text" name="email" placeholder="E-mail" required>
                 <input type="submit" onclick="add()" value="Agregar">
-        </form>
-    </div>
-    <script>
-        
-        function del()
-        {
-            Swal.fire(
-                'Eliminado',
-                'Click para continuar',
-                'success'
+            </form>
+        </div>
+        <script>
+            function del() {
+                Swal.fire(
+                    'Eliminado',
+                    'Click para continuar',
+                    'success'
                 )
-        }
-        function add()
-        {
-            Swal.fire(
-                'Cliente Agregado',
-                'Click para continuar',
-                'success'
+            }
+
+            function add() {
+                Swal.fire(
+                    'Cliente Agregado',
+                    'Click para continuar',
+                    'success'
                 )
-        }
-    </script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+            }
+        </script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </body>
 </div>
-<div class="users-table">
-    <br>
-    <h2>Pacientes registrados</h2>
-    <br>
-    <table>                     <!--creamos la tabla para mostrar los clientes creados-->
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>DNI</th>
-                <th>NOMBRE</th>
-                <th>NUMERO DE TELEFONO</th>
-                <th>DIRECCION</th>
-                <th>E-MAIL</th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($row = mysqli_fetch_array($query)): ?>
-                <tr>
-                    <td><?= $row['id'] ?></td>
-                    <td><?= $row['dni'] ?></td>
-                    <td><?= $row['name'] ?></td>
-                    <td><?= $row['phone_number'] ?></td>
-                    <td><?= $row['address'] ?></td>
-                    <td><?= $row['email'] ?></td>
-                    <td><a href="update-patients.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></td>     
-                    <td><a onclick="del()" href="delete-patients.php?id=<?= $row['id'] ?>" class="users-table--delete">Eliminar</a></td> 
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-    <br>
-    <br>
+<br>
+<h2>Pacientes registrados</h2>
+<br>
+<div class="container">
+    <div class="table-responsive">
+        <table class="table table-light">
+            <!--creamos la tabla para mostrar los clientes creados-->
+
+            <div class="users-table">
+                <thead class="table-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>DNI</th>
+                        <th>NOMBRE</th>
+                        <th>NUMERO DE TELEFONO</th>
+                        <th>DIRECCION</th>
+                        <th>E-MAIL</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_array($query)) : ?>
+                        <tr>
+                            <td><?= $row['id'] ?></td>
+                            <td><?= $row['dni'] ?></td>
+                            <td><?= $row['name'] ?></td>
+                            <td><?= $row['phone_number'] ?></td>
+                            <td><?= $row['address'] ?></td>
+                            <td><?= $row['email'] ?></td>
+                            <td><a href="update-patients.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></td>
+                            <td><a onclick="del()" href="delete-patients.php?id=<?= $row['id'] ?>" class="users-table--delete">Eliminar</a></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+
+            </div>
+        </table>
+    </div>
 </div>
 
 </html>
--- registration-servicePatients.php
+++ registration-servicePatients.php
@@ -1,116 +1,121 @@
 <?php
-    //Registration-Servicios Clientes
-    include("config.php");
-    include("session.php");
+//Registration-Servicios Clientes
+include("config.php");
+include("session.php");
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
+
+<head>
     <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Registro de Servicio Paciente</title>
-        <link rel="stylesheet" type="text/css" href="style-users.css?ts=<?=time()?>" />
-        <meta name=""viewport content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
-        
-    </head>
-    
-    <div>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Registro de Servicio Paciente</title>
+    <link rel="stylesheet" type="text/css" href="style-servicePatients.css" />
+    <meta name="" viewport content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
+</head>
+
+<div>
     <?php
-include("header.php"); 
+    include("header.php");
     ?>
 </div>
 <div>
+
     <body>
-    <script>
-        function add()
-        {
-            Swal.fire(
-                'Servicio Agregado',
-                'Click para continuar',
-                'success'
+        <script>
+            function add() {
+                Swal.fire(
+                    'Servicio Agregado',
+                    'Click para continuar',
+                    'success'
                 )
-        }
-    </script>
-    <div class="users-form">
-        <form action="register-servicePatients.php" method="POST">
-            <h1>Ingresar datos de Servicio Paciente</h1>
-            <input type="text" name="id_users" placeholder="Id Usuario" list="busqueda_u">
-            <datalist id="busqueda_u">
-                <option>
-                <?php 
-                    $sql = "SELECT * FROM users"; 
-                    $query = mysqli_query($mysqli, $sql);
+            }
+        </script>
+        <div class="users-form">
+            <form action="register-servicePatients.php" method="POST">
+                <h1>Ingresar datos de Servicio Paciente</h1>
+                <input type="text" name="id_users" placeholder="Id Usuario" list="busqueda_u">
+                <datalist id="busqueda_u">
+                    <option>
+                        <?php
+                        $sql = "SELECT * FROM users";
+                        $query = mysqli_query($mysqli, $sql);
 
-                while ($row = mysqli_fetch_array($query)): ?>
-                        <option><?= $row['id'] ?></option>
+                        while ($row = mysqli_fetch_array($query)) : ?>
+                    <option><?= $row['id'] ?></option>
                 <?php endwhile; ?>
                 </option>
-            </datalist>
-            <input type="text" name="id_patients" placeholder="Id Patients" list="busqueda_p">
-            <datalist id="busqueda_p">
-                <option>
-                <?php 
-                    $sql = "SELECT * FROM patients"; 
-                    $query = mysqli_query($mysqli, $sql);
+                </datalist>
+                <input type="text" name="id_patients" placeholder="Id Patients" list="busqueda_p">
+                <datalist id="busqueda_p">
+                    <option>
+                        <?php
+                        $sql = "SELECT * FROM patients";
+                        $query = mysqli_query($mysqli, $sql);
 
-                while ($row = mysqli_fetch_array($query)): ?>
-                        <option><?= $row['id'] ?></option>
+                        while ($row = mysqli_fetch_array($query)) : ?>
+                    <option><?= $row['id'] ?></option>
                 <?php endwhile; ?>
                 </option>
-            </datalist>
-            <input type="text" name="id_service" placeholder="Id Service" list="busqueda_s">
-            <datalist id="busqueda_s">
-                <option>
-                <?php 
-                    $sql = "SELECT * FROM services"; 
-                    $query = mysqli_query($mysqli, $sql);
+                </datalist>
+                <input type="text" name="id_service" placeholder="Id Service" list="busqueda_s">
+                <datalist id="busqueda_s">
+                    <option>
+                        <?php
+                        $sql = "SELECT * FROM services";
+                        $query = mysqli_query($mysqli, $sql);
 
-                while ($row = mysqli_fetch_array($query)): ?>
-                        <option><?= $row['id'] ?></option>
+                        while ($row = mysqli_fetch_array($query)) : ?>
+                    <option><?= $row['id'] ?></option>
                 <?php endwhile; ?>
                 </option>
-            </datalist>
-            <input type="date" placeholder="Fecha de Entrega" name="date_of_delivery">
-            <input type="submit" onclick="add()" value="Agregar">
-        </form> 
-    </div>  
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                </datalist>
+                <input type="date" placeholder="Fecha de Entrega" name="date_of_delivery">
+                <input type="submit" onclick="add()" value="Agregar">
+            </form>
+        </div>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </body>
 </div>
-<div class="users-table">
-    <br>
-    <Registrados>Servicios Pacientes Registrados</h2>
-    <br>
-    <table>                     <!--creamos la tabla para mostrar los clientes creados-->
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>ID_USUARIOS</th>
-                <th>ID_PACIENTES</th>
-                <th>ID_SERVICIOS</th>
-                <th>FECHA DE ENTREGA</th>
-                <th>FECHA/HORA REALIZADA</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-            $sql = "SELECT * FROM servicepatients"; 
-            $query = mysqli_query($mysqli, $sql);
-            while ($row = mysqli_fetch_array($query)): ?>
-                <tr>
-                    <td><?= $row['id_sp'] ?></td>
-                    <td><?= $row['id_u'] ?></td>
-                    <td><?= $row['id_p'] ?></td>
-                    <td><?= $row['id_s'] ?></td>
-                    <td><?= $row['date_of_delivery'] ?></td>
-                    <td><?= $row['date'] ?></td>
-                </tr>             
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-    <br>
-    <br>
-</div>
 
-</html>  
+<br>
+<Registrados>Servicios Pacientes Registrados</h2>
+    <br>
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table table-light">
+                <div class="users-table">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>ID_USUARIOS</th>
+                            <th>ID_PACIENTES</th>
+                            <th>ID_SERVICIOS</th>
+                            <th>FECHA DE ENTREGA</th>
+                            <th>FECHA/HORA REALIZADA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM servicepatients";
+                        $query = mysqli_query($mysqli, $sql);
+                        while ($row = mysqli_fetch_array($query)) : ?>
+                            <tr>
+                                <td><?= $row['id_sp'] ?></td>
+                                <td><?= $row['id_u'] ?></td>
+                                <td><?= $row['id_p'] ?></td>
+                                <td><?= $row['id_s'] ?></td>
+                                <td><?= $row['date_of_delivery'] ?></td>
+                                <td><?= $row['date'] ?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </div>
+            </table>
+            <br>
+            <br>
+        </div>
+    </div>
+
+</html>
